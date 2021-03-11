2fc3691e2ae6cafe9c518bf3e9d6b79e2eaac9c9
noriko1599 <sminko1599@gmail.com>
2021-03-11 15:48:01 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+// pm2-runtime is great at running node apps inside containers properly,
+// but its arguments can be mistaken for a command line Apostrophe task
+if (process.argv.find(arg => arg.match(/ProcessContainer/))) {
+  process.argv = [process.argv[0], 'app.js'];
+}
+
+const apos = require('apostrophe')({
+  shortName: 'saros-apos-development',
+
+  // See lib/modules for basic project-level configuration of our modules
+  // responsible for serving static assets, managing page templates and
+  // configuring user accounts.
+
+  modules: {
+
+    // Apostrophe module configuration
+
+    // Note: most configuration occurs in the respective
+    // modules' directories. See lib/apostrophe-assets/index.js for an example.
+    // However any modules that are not present by default in Apostrophe must at
+    // least have a minimal configuration here: `moduleName: {}`
+
+    // If a template is not found somewhere else, serve it from the top-level
+    // `views/` folder of the project
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+
+    // Add an alt field to images schema, by default the title is used but
+    // we recommend enabling the alt field for clarity.
+    'apostrophe-images': {
+      enableAltField: true
+    },
+
+    'apostrophe-db': {
+      uri: 'mongodb+srv://admin:dope@cluster0.f2tlu.mongodb.net/apostrophe-sandbox'
+      // There is legacy support for host, port, name, user and password options,
+      // but this is not necessary. They can all go in the uri option like this:
+      // mongodb://user:password@host:port/dbname
+    }
+  }
+});
